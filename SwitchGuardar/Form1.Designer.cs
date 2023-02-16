9ff19696027f7f9551018dda674b4318ea2200ad
CCBB <CCBB@CCBB-23>
2023-02-16 11:08:15 -0600

    Agregar archivos de proyecto.

---
+++ SwitchGuardar/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace SwitchGuardar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dvgdatos = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cbotipoformato = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgdatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dvgdatos
+            // 
+            this.dvgdatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgdatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.lastname,
+            this.phone});
+            this.dvgdatos.Location = new System.Drawing.Point(136, 193);
+            this.dvgdatos.Name = "dvgdatos";
+            this.dvgdatos.Size = new System.Drawing.Size(341, 150);
+            this.dvgdatos.TabIndex = 0;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Nombre";
+            this.name.Name = "name";
+            // 
+            // lastname
+            // 
+            this.lastname.HeaderText = "Apellido";
+            this.lastname.Name = "lastname";
+            // 
+            // phone
+            // 
+            this.phone.HeaderText = "Telefono";
+            this.phone.Name = "phone";
+            // 
+            // cbotipoformato
+            // 
+            this.cbotipoformato.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbotipoformato.FormattingEnabled = true;
+            this.cbotipoformato.Items.AddRange(new object[] {
+            "Excel",
+            "PDF",
+            "TXT"});
+            this.cbotipoformato.Location = new System.Drawing.Point(223, 349);
+            this.cbotipoformato.Name = "cbotipoformato";
+            this.cbotipoformato.Size = new System.Drawing.Size(125, 21);
+            this.cbotipoformato.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 354);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tipo de Formato:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbotipoformato);
+            this.Controls.Add(this.dvgdatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dvgdatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dvgdatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phone;
+        private System.Windows.Forms.ComboBox cbotipoformato;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SwitchGuardar/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using objExcel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Reflection;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Runtime.ConstrainedExecution;
+using File = System.IO.File;
+
+namespace SwitchGuardar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dvgdatos.Rows.Add("Leandro", "Lacayo", "83710688");
+            dvgdatos.Rows.Add("Adriana", "Lopez", "83842742");
+            dvgdatos.Rows.Add("Hollman", "Lacayo", "87759205");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int indice = cbotipoformato.SelectedIndex;
+
+
+            switch (indice)
+            {
+
+                case 0:
+                    objExcel.Application objAplicacion = new objExcel.Application();
+                    Workbook objLibro = objAplicacion.Workbooks.Add(XlSheetType.xlWorksheet);
+                    Worksheet objHoja = (Worksheet)objAplicacion.ActiveSheet;
+
+                    objAplicacion.Visible = false;
+
+
+
+                    foreach (DataGridViewColumn columna in dvgdatos.Columns)
+                    {
+                        objHoja.Cells[1, columna.Index + 1] = columna.HeaderText;
+                        foreach (DataGridViewRow fila in dvgdatos.Rows)
+                        {
+                            objHoja.Cells[fila.Index + 2, columna.Index + 1] = fila.Cells[columna.Index].Value;
+                        }
+                    }
+
+                    objLibro.SaveAs(ruta + "\\Prueba.xlsx");
+                    objLibro.Close();
+                    MessageBox.Show("Se creo el archivo excel correctamente");
+
+                    break;
+
+
+                case 1:
+
+                    PdfPTable pdfTable = new PdfPTable(dvgdatos.ColumnCount);
+
+                    pdfTable.DefaultCell.Padding = 3;
+
+                    pdfTable.WidthPercentage = 70;
+                    pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+                    pdfTable.DefaultCell.BorderWidth = 1;
+
+                    foreach (DataGridViewColumn column in dvgdatos.Columns)
+                    {
+                        PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                        cell.BackgroundColor = new iTextSharp.text.BaseColor(240, 240, 240);
+                        pdfTable.AddCell(cell);
+                    }
+                    foreach (DataGridViewRow row in dvgdatos.Rows)
+                    {
+                        foreach (DataGridViewCell cell in row.Cells)
+                        {
+                            if (cell.Value == null)
+                            {
+
+                            }
+                            else
+                            {
+                                pdfTable.AddCell(cell.Value.ToString());
+
+                            }
+
+
+                        }
+                    }
+
+                    string folderPath = "C:\\Users\\CCBB\\desktop\\Archivo PDF\\";
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                    }
+                    using (FileStream stream = new FileStream(folderPath + "Archivo.pdf", FileMode.Create))
+                    {
+
+
+
+
+                        Document pdfDoc = new Document(PageSize.A2, 10f, 10f, 10f, 0f);
+
+                        PdfWriter.GetInstance(pdfDoc, stream);
+                        pdfDoc.Open();
+                        pdfDoc.Add(pdfTable);
+                        pdfDoc.Close();
+                        stream.Close();
+                        MessageBox.Show("Se creo el archivo pdf correctamente");
+                    }
+                    break;
+
+
+
+                case 2:
+                    
+                    
+                        StreamWriter escribir = new StreamWriter("C:\\Users\\CCBB\\desktop\\ArchivoGod.txt", true);
+
+                        foreach (DataGridViewRow row in dvgdatos.Rows)
+                        {
+                            escribir.WriteLine($"{row.Cells[0].Value}--{row.Cells[1].Value}--{row.Cells[2].Value}");
+                        }
+                        MessageBox.Show("Se creo el archivo txt");
+                        escribir.Close();
+                        break;
+                   
+            }
+        }
+    }
+}
+
